a6061552b94870adbd30e2a7d65dcfb6944e83a8
Caio César Valério <ccmvcaio@gmail.com>
2020-08-17 20:41:55 -0300

    Validando categorias de carros

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -12,8 +12,12 @@ class CarCategoriesController < ApplicationController
   end
 
   def create
-    @car_category = CarCategory.create(car_category_params)
-    redirect_to @car_category     #Direciona a página para o objeto que foi criado
+    @car_category = CarCategory.new(car_category_params)
+    if @car_category.save
+      redirect_to @car_category
+    else
+      render :new
+    end
   end
 
   private
