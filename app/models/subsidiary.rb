74b73b773e30c749387b034acfff232b393ec78a
Caio César Valério <ccmvcaio@gmail.com>
2020-08-18 13:34:14 -0300

    Admin edita filial

--- app/models/subsidiary.rb
+++ app/models/subsidiary.rb
@@ -1,4 +1,4 @@
 class Subsidiary < ApplicationRecord
   validates :name, :cnpj, :address, presence: true
-  validates :name, uniqueness: {case_sensitive: false}
+  validates :name, :cnpj, :address, uniqueness: {case_sensitive: false}
 end
