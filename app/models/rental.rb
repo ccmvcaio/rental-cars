b1dbf97697e1e7b2b83472c4289e5231d55e90eb
Caio César Valério <ccmvcaio@gmail.com>
2020-09-02 18:23:43 -0300

    Gerando token para Locação

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,4 +3,11 @@ class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :car_category
   validates :start_date, :end_date, :client, :car_category, presence: true
+
+  before_create :generate_token
+
+  private
+  def generate_token
+    self.token = SecureRandom.alphanumeric(6)
+  end
 end
--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Rental, type: :model do
-  
+  context 'token' do
+    it 'generate token on create' do
+    user = User.create!(name: 'Caio Valério', email: 'caio.valerio@gmail.com',
+                        password: '123456')
+    car_category = CarCategory.create!(name: 'Econo', daily_rate: 105.5,
+                                       car_insurance: 58.5, third_party_insurance: 10.5)
+    client = Client.create!(name: 'Santos Silva', email: 'santos.silva@gmail.com',
+                            cpf: '418.882.130-27')
+    rental = Rental.create!(start_date: Date.current, end_date: 1.day.from_now,
+                            car_category: car_category, user: user, client: client)
+
+    rental.save!
+    rental.reload
+
+    expect(rental.token).to be_present
+    expect(rental.token.length).to eq(6)
+    expect(rental.token).to match(/^[A-Z0-9]+$/)
+    end
+  end
 end
