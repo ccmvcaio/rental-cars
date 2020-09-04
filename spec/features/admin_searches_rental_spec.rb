8126e4a9498ea59c632e01124f01c67cae9f2311
Caio César Valério <ccmvcaio@gmail.com>
2020-09-04 13:46:39 -0300

    Admin procura por token parcial

--- spec/features/admin_searches_rental_spec.rb
+++ spec/features/admin_searches_rental_spec.rb
@@ -27,7 +27,32 @@ feature 'Admin searches rental' do
     expect(page).to have_content(rental.user.email)
   end
 
-  xscenario 'and finds nothing' do
+  scenario 'and find by partial token' do
+    user = User.create!(name: 'Caio Valério', email: 'caio.valerio@gmail.com',
+                        password: '123456')
+    car_category = CarCategory.create!(name: 'Econo', daily_rate: 105.5,
+                                       car_insurance: 58.5, third_party_insurance: 10.5)
+    client = Client.create!(name: 'Santos Silva', email: 'santos.silva@gmail.com',
+                            cpf: '418.882.130-27')
+    rental = Rental.create!(start_date: Date.current, end_date: 1.day.from_now,
+                            car_category: car_category, user: user, client: client)
+    rental.update(token: 'ABC123')
+    second_rental = Rental.create!(start_date: Date.current, end_date: 1.day.from_now,
+                                   car_category: car_category, user: user, 
+                                   client: client)
+    second_rental.update(token: 'ABC222')
+    third_rental = Rental.create!(start_date: Date.current, end_date: 1.day.from_now,
+                                   car_category: car_category, user: user, 
+                                   client: client)
 
+    login_as(user, scope: :user)
+    visit root_path
+    click_on 'Locações'
+    fill_in 'Buscar locação', with: 'ABC'
+    click_on 'Buscar'
+
+    expect(page).to have_content(rental.token)
+    expect(page).to have_content(second_rental.token)
+    expect(page).to_not have_content(third_rental)
   end
 end
